079f2d2aef6dc001cd540e8f080da7b621accf3b
jamezme <100404570+jamezme@users.noreply.github.com>
2024-03-16 10:16:59 -0700

    Added all comments

--- main.asm
+++ main.asm
@@ -1,8 +1,5 @@
 
 ;***********************************************************
-;*
-;*	This is the TRANSMIT skeleton file for Lab 7 of ECE 375
-;*
 ;*  	Rock Paper Scissors
 ;* 	Requirement:
 ;* 	1. USART1 communication
@@ -10,7 +7,7 @@
 ;***********************************************************
 ;*
 ;*	 Author: Murphy James and Owen Wheary
-;*	   Date: 03/07/2024
+;*	   Date: 03/16/2024
 ;*
 ;***********************************************************
 
@@ -20,7 +17,7 @@
 ;*  Internal Register Definitions and Constants
 ;***********************************************************
 .def    mpr = r16               ; Multi-Purpose Register
-.def	state = r17				; State of game 
+.def	state = r17				; State of opponent 
 .def	play = r18				; state of choice 
 .def	waitcnt = r19
 .def	ilcnt = r23
@@ -29,26 +26,23 @@
 ;***********************************************************
 ;*  Internal Register Definitions and Constants
 ;***********************************************************
-.macro	WRITE_LINE_1
-	ldi		ZL, low(@0 << 1)
+.macro	WRITE_LINE_1			; Macro for quick writing to LCD Display Line 1
+	ldi		ZL, low(@0 << 1)	; Set Z to beginning of program memory address
 	ldi		ZH, high(@0 << 1)
-	ldi		YL, $00
+	ldi		YL, $00				; Set Y to the beginning of Data Memory of Line 1
 	ldi		YH, $01
 
-	call	LineWrite
+	call	LineWrite			; Call a helper function to copy data over 
 .endm
 
-.macro	WRITE_LINE_2
-	ldi		ZL, low(@0 << 1)
+.macro	WRITE_LINE_2			; Mecro for quick writing to LCD Display Line 2
+	ldi		ZL, low(@0 << 1)	; Set Z to beginnning of program memory address
 	ldi		ZH, high(@0 << 1)
-	ldi		YL, $10
+	ldi		YL, $10				; Set Y to the beginning of Data Memory of Line 2 
 	ldi		YH, $01
 
-	call	LineWrite
+	call	LineWrite			; Call a helper function to copy over data
 .endm
-
-; Use this signal code between two boards for their game ready
-.equ    SendReady = 0b11111111
 
 ;***********************************************************
 ;*  Start of Code Segment
@@ -61,16 +55,12 @@
 .org    $0000                   ; Beginning of IVs
 	    rjmp    INIT            	; Reset interrupt
 
-.org	$0002
-		rcall	ChoiceSelect
+.org	$0002					; INT0 
+		rcall	ChoiceSelect 	; Function to change choice during Game Start
 		reti
 
-.org	$0028
-;		rcall	TimeOut
-		reti
-
-.org	$0032
-		rcall	DataReceived
+.org	$0032					; Receive Interrupt
+		rcall	DataReceived	; Function to save received data 
 		reti
 
 .org    $0056                   ; End of Interrupt Vectors
@@ -99,19 +89,19 @@ INIT:
 		out		PORTD, mpr		; so all Port D inputs are Tri-State
 
 	;USART1
-		ldi		mpr, 0b0010_0010
+		ldi		mpr, 0b0010_0010	; Enable double speed 
 		sts		UCSR1A, mpr
 
-		ldi		mpr, 0b1001_1000
+		ldi		mpr, 0b1001_1000	; Enable 8 bits and receive interrupt
 		sts		UCSR1B, mpr 
 
-		ldi		mpr, 0b0000_1110
+		ldi		mpr, 0b0000_1110	; Asynchronous, 2 stop bits, no parity 
 		sts		UCSR1C, mpr 
 
-		ldi		mpr, 0b0000_0001
+		ldi		mpr, 0b0000_0001	; Set BAUD rate to 2400, registers set to 416
 		sts		UBRR1H, mpr
 
-		ldi		mpr, 0b10100000
+		ldi		mpr, 0b10100000		; lower half of 416
 		sts		UBRR1L, mpr
 		;Set baudrate at 2400bps
 		;Enable receiver and transmitter
@@ -123,7 +113,7 @@ INIT:
 			; Set the Interrupt Sense Control to falling edge
 
 		; Configure the External Interrupt Mask
-		ldi		mpr, 0b0000_0000	; configure INT0, 1
+		ldi		mpr, 0b0000_0000	; disable button until StartGame
 		out		EIMSK, mpr			; send to register
 
 
@@ -134,34 +124,34 @@ INIT:
 		sts		TCCR1B, mpr			; Normal Mode, prescale 1024
 		
 	;Other
-		ldi		XH, high(OP_READY)
+		ldi		XH, high(OP_READY)	; Clear out opponent status/play
 		ldi		XL, low(OP_READY)
 		ldi		mpr, $00
 		st		X, mpr
 
-		ldi		XH, high(PLAYCHOICE)
+		ldi		XH, high(PLAYCHOICE) ; Clear out user status/play 
 		ldi		XL, low(PLAYCHOICE)
 		ldi		mpr, $00
 		st		X, mpr
 
-		clr		play
-		ldi		waitcnt, $03
+		clr		play				; Clear play register 
+		ldi		waitcnt, $03		; set debounce wait to 30ms
 
-		call	LCDInit
-		call	LCDBacklightOn
+		call	LCDInit				; initialize LCDDisplay 
+		call	LCDBacklightOn	
 
-		sei
+		sei							; Enable global interrupts
 
 ;***********************************************************
 ;*  Main Program
 ;***********************************************************
 MAIN:
-	rcall	WelcomePlayer
-	rcall	GetReady
-	rcall	StartGame
-	rcall	SendChoice
-	rcall	DisplayResults
-	rjmp	MAIN
+	rcall	WelcomePlayer	; Function call to welcome sequence
+	rcall	GetReady		; Function call to handling readying up 
+	rcall	StartGame		; Function call to handle choosing a move 
+	rcall	SendChoice		; Function call to send and receive chosen move 
+	rcall	DisplayResults	; Function call to display win, lose, draw
+	rjmp	MAIN			; repeat forever 
 
 ;***********************************************************
 ;*	Functions and Subroutines
@@ -169,47 +159,50 @@ MAIN:
 
 ;-----------------------------------------------------------
 ; Func:	WelcomePlayer
-; Desc:	Changes play choice from Rock, Paper, or Scissors 
+; Desc:	Displays the welcome message until the user clicks 
+;	PD7 indicating readiness. 
 ;-----------------------------------------------------------
 WelcomePlayer:
-	WRITE_LINE_1		PROG_START
-	WRITE_LINE_2		PROG_START2
-	rcall	LCDWrite
+	WRITE_LINE_1		PROG_START	; Write Welcome message
+	WRITE_LINE_2		PROG_START2	
+	rcall	LCDWrite				; push to screen 
 
 Check:
-	in		mpr, PIND
+	in		mpr, PIND	; poll for PD7 button press 
 	sbrc	mpr, 7
 	rjmp	Check
 
 	ret
 ;-----------------------------------------------------------
 ; Func:	GetReady
-; Desc:	Changes play choice from Rock, Paper, or Scissors 
+; Desc:	Writes ready message to display, sends ready message 
+; 	to other board, and polls to receive opponent's ready 
+; 	signal.
 ;-----------------------------------------------------------
 GetReady:
-	push	mpr
+	push	mpr		; push registers onto stack 
 	push	XH
 	push	XL
 	push	state
-	WRITE_LINE_1		READY
+	WRITE_LINE_1		READY	; Write ready message to data memory 
 	WRITE_LINE_2		READY2
-	rcall	LCDWrite 
+	rcall	LCDWrite 			; push to display 
 
-RU:	lds		mpr, UCSR1A ; Check if Transmitter is ready
-	sbrs	mpr, UDRE1 ; Data Register Empty flag
-	rjmp	RU ; Loop until UDR1 is empty
+RU:	lds		mpr, UCSR1A 	; Check if Transmitter is ready
+	sbrs	mpr, UDRE1 		; Data Register Empty flag
+	rjmp	RU 				; Loop until UDR1 is empty
 
-	ldi		mpr, $FF
-	sts		UDR1, mpr ; Move data to transmit data buffer
+	ldi		mpr, $FF	; determined ready signal
+	sts		UDR1, mpr 	; Move data to transmit data buffer
 
-	ldi		XL, low(OP_READY)
+	ldi		XL, low(OP_READY)	; Set X to data memory of OP_READY
 	ldi		XH, high(OP_READY)
 ORDY:
-	ld		state, X
-	cpi		state, $FF
-	brne	ORDY
+	ld		state, X	; Load opponent's status 
+	cpi		state, $FF	; check if ready 
+	brne	ORDY		; loop if not 
 
-	pop		state
+	pop		state		; pop registers in reverse order 
 	pop		XL
 	pop		XH
 	pop		mpr
@@ -217,77 +210,81 @@ ORDY:
 	ret
 ;-----------------------------------------------------------
 ; Func:	StartGame
-; Desc:	Changes play choice from Rock, Paper, or Scissors 
+; Desc:	Writes Game start to the screen and handles the 
+; 	countdown. Enables and then disables PD4 interrupt. 
 ;-----------------------------------------------------------
 StartGame:
 	push	mpr
 
-	ldi		mpr, 0b0000_0001	; configure INT0, 1
+	ldi		mpr, 0b0000_0001	; Enable PD4 button interrupt
 	out		EIMSK, mpr			; send to register
 
 	cli
-	rcall LCDClr
-	WRITE_LINE_1	GAME_START
+	rcall LCDClr				; clear the screen 
+	WRITE_LINE_1	GAME_START	; Move start message to line 1 DM 
 	rcall	LCDInit
-	rcall	LCDWrite
+	rcall	LCDWrite			; Write it to screen 
 	sei
 
-	ldi		mpr, $F0
-	out		PORTB, mpr
-	rcall	WaitCount
-	cbi		PORTB, 7
-	rcall	WaitCount
-	cbi		PORTB, 6
-	rcall	WaitCount
-	cbi		PORTB, 5
-	rcall	WaitCount
-	cbi		PORTB, 4
+	ldi		mpr, $F0		; Turn on PORTB LEDs
+	out		PORTB, mpr		
+	rcall	WaitCount		; Wait 1.5s
+	cbi		PORTB, 7		; Turn off an LED
+	rcall	WaitCount		; Wait 1.5s
+	cbi		PORTB, 6		; Turn off an LED
+	rcall	WaitCount		; Wait 1.5s
+	cbi		PORTB, 5		; Turn off an LED
+	rcall	WaitCount		; Wait 1.5s
+	cbi		PORTB, 4		; Turn off an LED
 
-	ldi		mpr, 0	; configure INT0, 1
-	out		EIMSK, mpr			; send to register
+	ldi		mpr, 0		; disable PD4 interrupt
+	out		EIMSK, mpr	; send to register
 
 	pop		mpr
 
 	ret
 ;-----------------------------------------------------------
 ; Func:	WaitCount
-; Desc:	Changes play choice from Rock, Paper, or Scissors 
+; Desc:	Uses Timer/Counter1 to wait for 1.5s. 
 ;-----------------------------------------------------------
 WaitCount:
 	push	mpr
 
-	sbi		TIFR1, TOV1
-	ldi		mpr, high(53817)
+	sbi		TIFR1, TOV1		; reset the TOV flag
+	ldi		mpr, high(53817)	; load in value for 1.5s
 	sts		TCNT1H, mpr
 	ldi		mpr, low(53817)
 	sts		TCNT1L, mpr 
 
 DONE: 
-	sbis	TIFR1, 0
+	sbis	TIFR1, 0	; Wait for timer 
 	rjmp	DONE
-	sbi		TIFR1, TOV1
+	sbi		TIFR1, TOV1	; Reset the TOV flag 
 
 	pop		mpr
 	
 	ret
 ;-----------------------------------------------------------
 ; Func:	LineWrite
-; Desc:	Changes play choice from Rock, Paper, or Scissors 
+; Desc:	Helper function to macros that copies prog memory 
+;	to data memory. 
 ;-----------------------------------------------------------
 LineWrite:
 	push	mpr
-LW:	lpm		mpr, Z+
-	st		Y+, mpr
+LW:	lpm		mpr, Z+		; load ProgMem
+	st		Y+, mpr		; store in DM
 
-	mov		mpr, YL
+	mov		mpr, YL		; check if Y has reached F on lowest nibble 
 	andi	mpr, $0F
-	brne	LW
+	brne	LW			; loops until it has 
 	pop		mpr
 
 	ret
 ;-----------------------------------------------------------
 ; Func:	SendChoice
-; Desc:	Changes play choice from Rock, Paper, or Scissors 
+; Desc:	Transmit the users choice and polls for opponent's
+;	choice and then displays the choice. Additionally, handles
+;	countdown for this section. 
 ;-----------------------------------------------------------
 SendChoice:
 	push	mpr
@@ -297,56 +294,56 @@ SendChoice:
 	push	YH
 
 SC_Transmit:
-	lds		mpr, UCSR1A ; Check if Transmitter is ready
-	sbrs	mpr, UDRE1 ; Data Register Empty flag
-	rjmp	SC_Transmit ; Loop until UDR1 is empty
+	lds		mpr, UCSR1A 	; Check if Transmitter is ready
+	sbrs	mpr, UDRE1 		; Data Register Empty flag
+	rjmp	SC_Transmit 	; Loop until UDR1 is empty
 
-	ldi		XL, low(PLAYCHOICE)
+	ldi		XL, low(PLAYCHOICE)	; Load in players choice 
 	ldi		XH, high(PLAYCHOICE)
-	ld		mpr, X
-	sts		UDR1, mpr ; Move data to transmit data buffer
+	ld		mpr, X			; Send to mpr 
+	sts		UDR1, mpr 		; Move data to transmit data buffer
 
 SC_DISPLAY:
-	ldi		YL, low(OP_READY)
+	ldi		YL, low(OP_READY)	; Load in opponents choice/status 
 	ldi		YH, high(OP_READY)
 	ld		mpr, Y
 	
-	cpi		mpr, $FF
-	breq	SC_DISPLAY
+	cpi		mpr, $FF		; If still the ready signal
+	breq	SC_DISPLAY		; continue looping 
 	
-	cpi		mpr, $01
+	cpi		mpr, $01		; If rock, display rock 
 	breq	DISPLAY_ROCK
 
-	cpi		mpr, $02
+	cpi		mpr, $02		; If paper, display paper 
 	breq	DISPLAY_PAPER
 
-	cpi		mpr, $04
+	cpi		mpr, $04		; If scissors, display scissors
 	breq	DISPLAY_SCISSORS
 
 DISPLAY_ROCK:
-	WRITE_LINE_1	ROCK
-	rjmp	THE_END
+	WRITE_LINE_1	ROCK	; Write line 1 as rock
+	rjmp	THE_END			; jump to wait 
 DISPLAY_PAPER:
-	WRITE_LINE_1	PAPER
-	rjmp	THE_END
+	WRITE_LINE_1	PAPER	; Write line 1 as paper
+	rjmp	THE_END			; jump to wait 
 DISPLAY_SCISSORS:
-	WRITE_LINE_1	SCISSORS
+	WRITE_LINE_1	SCISSORS	; Write line 1 as scissors 
 THE_END:
 	
 	cli
-	rcall	LCDWrite
+	rcall	LCDWrite		; Push to display 
 	sei
 
-	ldi		mpr, $F0
-	out		PORTB, mpr
-	rcall	WaitCount
-	cbi		PORTB, 7
-	rcall	WaitCount
-	cbi		PORTB, 6
-	rcall	WaitCount
-	cbi		PORTB, 5
-	rcall	WaitCount
-	cbi		PORTB, 4
+	ldi		mpr, $F0		; Turn on PORTB LEDs
+	out		PORTB, mpr		
+	rcall	WaitCount		; Wait 1.5s
+	cbi		PORTB, 7		; Turn off an LED
+	rcall	WaitCount		; Wait 1.5s
+	cbi		PORTB, 6		; Turn off an LED
+	rcall	WaitCount		; Wait 1.5s
+	cbi		PORTB, 5		; Turn off an LED
+	rcall	WaitCount		; Wait 1.5s
+	cbi		PORTB, 4		; Turn off an LED
 
 	pop		YH
 	pop		YL
@@ -358,46 +355,47 @@ THE_END:
 ;-----------------------------------------------------------
 ; Func:	ChoiceSelect
 ; Desc:	Changes play choice from Rock, Paper, or Scissors 
+;	upon PD4 button press interrupt. 
 ;-----------------------------------------------------------
 ChoiceSelect:	
 	push	play
 	push	mpr
-	ldi		XH, high(PLAYCHOICE)
+	ldi		XH, high(PLAYCHOICE)	; Load in address to player's choice
 	ldi		XL, low(PLAYCHOICE)
 
-	ld		play, X
+	ld		play, X		; load into play register 
 
-	cpi		play, $00
+	cpi		play, $00	; If first press, default to rock
 	breq	CS_ROCK
 	
-	cpi		play, $04
+	cpi		play, $04	; If at scissors, go back to rock 
 	breq	CS_ROCK
 
-	cpi		play, $01
+	cpi		play, $01	; If at rock, go to paper
 	breq	CS_PAPER
 
-	cpi		play, $02
+	cpi		play, $02	; If at paper, go to scissors 
 	breq	CS_SCISSORS
 
 CS_ROCK:
-	ldi		play, $01
-	WRITE_LINE_2	ROCK
-	rjmp	CS_END
+	ldi		play, $01	; Load in rock 
+	WRITE_LINE_2	ROCK ; Write to data memory 
+	rjmp	CS_END	; Jump to end 
 CS_PAPER:
-	ldi		play, $02
-	WRITE_LINE_2	PAPER
-	rjmp	CS_END
-CS_SCISSORS:
-	ldi		play, $04
-	WRITE_LINE_2	SCISSORS
+	ldi		play, $02	; Load in paper 
+	WRITE_LINE_2	PAPER	; Write to data memory 
+	rjmp	CS_END		; Jump to end 
+CS_SCISSORS:	
+	ldi		play, $04	; Load in scissors 
+	WRITE_LINE_2	SCISSORS	; Write to data memory 
 
 CS_END:
-	st		X, play
+	st		X, play		; Store new choice into PLAYCHOICE 
 	cli
-	rcall	LCDWrite
-	rcall	WaitClr
+	rcall	LCDWrite	; Display new choice 
+	rcall	WaitClr		; Debouncing action 
 	sei
-	ldi		mpr, $FF
+	ldi		mpr, $FF	; reset interrupts 
 	out		EIMSK, mpr
 	pop		mpr
 	pop		play
@@ -405,7 +403,7 @@ CS_END:
 	ret
 
 ;----------------------------------------------------------------
-; Sub:	Wait
+; Sub:	WaitClr
 ; Desc:	A wait loop that is 16 + 159975*waitcnt cycles or roughly
 ;		waitcnt*10ms.  Just initialize wait for the specific amount
 ;		of time in 10ms intervals. Here is the general eqaution
@@ -421,9 +419,9 @@ Loop:	ldi		olcnt, 224		; load olcnt register
 OLoop:	ldi		ilcnt, 237		; load ilcnt register
 ILoop:	dec		ilcnt			; decrement ilcnt
 		brne	ILoop			; Continue Inner Loop
-		dec		olcnt		; decrement olcnt
+		dec		olcnt			; decrement olcnt
 		brne	OLoop			; Continue Outer Loop
-		dec		waitcnt		; Decrement wait
+		dec		waitcnt			; Decrement wait
 		brne	Loop			; Continue Wait loop
 
 		pop		olcnt		; Restore olcnt register
@@ -433,19 +431,20 @@ ILoop:	dec		ilcnt			; decrement ilcnt
 
 ;-----------------------------------------------------------
 ; Func:	DataReceived
-; Desc:	 
+; Desc:	 Responds to USART receive interrupt. Loads in data 
+; from UDR1 to OP_READY. 
 ;-----------------------------------------------------------
 DataReceived:
 	push	mpr
 	push	XH
 	push	XL
 
-	lds		mpr, UDR1
+	lds		mpr, UDR1	; Load in received data 
 
-	ldi		XH, high(OP_READY)
+	ldi		XH, high(OP_READY)	; Load DM address 
 	ldi		XL, low(OP_READY)
 
-	st		X, mpr
+	st		X, mpr		; Store received into OP_READY 
 	
 	pop		XL
 	pop		XH
@@ -455,73 +454,70 @@ DataReceived:
 
 ;-----------------------------------------------------------
 ; Func:	DisplayResults
-; Desc:	 
+; Desc:	 Handles determining and displaying win, lose or draw.
+; 	Uses the 6 second WaitCount. 
 ;-----------------------------------------------------------
 DisplayResults:
 	cli	
-	rcall LCDClr
+	rcall LCDClr	; Clear the screen 
 	sei
-	ldi		XH, high(OP_READY)
+	ldi		XH, high(OP_READY)	; Load in opponents choice 
 	ldi		XL, low(OP_READY)
 	ld		r17, X
 
-	ldi		XH, high(PLAYCHOICE)
+	ldi		XH, high(PLAYCHOICE)	; Load in user's choice 
 	ldi		XL, low(PLAYCHOICE)
 	ld		play, X
 
-	cp		play, r17
-	breq	R_DRAW
+	cp		play, r17		; If they are the same choice 
+	breq	R_DRAW			; Display draw 
 
-	cpi		r17, 0
+	cpi		r17, 0			; If opponent never selected anything
 	brne	Do_Operation
-	ldi		r17, 1
+	ldi		r17, 1			; Assume rock 
 
-	cpi		play, 0
+	cpi		play, 0			; If user never selected anything
 	brne	Do_Operation
-	ldi		play, 1
+	ldi		play, 1			; Assume rock
 
 Do_Operation:
 
-	sbrc	r17, 0	; if the first bit is set (played rock)...
-	ori		r17, 0b1000	; set a bit out
-	lsr		r17		; and shift into position
+	sbrc	r17, 0		; If the first bit is set (played rock)...
+	ori		r17, 0b1000	; Set a bit out
+	lsr		r17			; and shift into position
 
-	cp		play, r17	; if play beats the opponent
-	breq	R_LOSE
+	cp		play, r17	; If play loses to the opponent
+	breq	R_LOSE		; Display lose 
 
-	rjmp	R_WIN
+	rjmp	R_WIN		; Display Win 
 
 R_DRAW:
-	WRITE_LINE_1	DRAW
-	rjmp	GAME_END
+	WRITE_LINE_1	DRAW	; Write Line 1 with draw message 
+	rjmp	GAME_END		; Jump to end 
 R_WIN:
-	WRITE_LINE_1	WIN
-	rjmp	GAME_END
+	WRITE_LINE_1	WIN		; Write line 1 with win message 
+	rjmp	GAME_END		; Jump to end 
 R_LOSE:
-	WRITE_LINE_1	LOSE
+	WRITE_LINE_1	LOSE	; Write Line 1 with lose message 
 GAME_END:
 	cli	
-	rcall LCDWrite
+	rcall LCDWrite			; Push to display 
 	sei
-	ldi		mpr, $F0
-	out		PORTB, mpr
-	rcall	WaitCount
-	cbi		PORTB, 7
-	rcall	WaitCount
-	cbi		PORTB, 6
-	rcall	WaitCount
-	cbi		PORTB, 5
-	rcall	WaitCount
-	cbi		PORTB, 4
+	ldi		mpr, $F0		; Turn on PORTB LEDs
+	out		PORTB, mpr		
+	rcall	WaitCount		; Wait 1.5s
+	cbi		PORTB, 7		; Turn off an LED
+	rcall	WaitCount		; Wait 1.5s
+	cbi		PORTB, 6		; Turn off an LED
+	rcall	WaitCount		; Wait 1.5s
+	cbi		PORTB, 5		; Turn off an LED
+	rcall	WaitCount		; Wait 1.5s
+	cbi		PORTB, 4		; Turn off an LED
 	ret
 ;***********************************************************
 ;*	Stored Program Data
 ;***********************************************************
 
-;-----------------------------------------------------------
-; An example of storing a string. Note the labels before and
-; after the .DB directive; these can help to access the data
-;-----------------------------------------------------------
 PROG_START:
     .DB		"Welcome!        "		; Declaring data in ProgMem
 PROG_START2:
@@ -560,7 +556,7 @@ DRAW:
 .org	$0130				; data memory allocation for opponent
 OP_READY:
 		.byte 1		
-PLAYCHOICE:
+PLAYCHOICE:					; data memory allocation for user 
 		.byte 1
 
 ;***********************************************************
